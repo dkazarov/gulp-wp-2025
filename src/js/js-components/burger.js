bcfa7406ea71e1ba392546a0f700b06fe9f5a637
unknown <d.kazarov1361@gmail.com>
2024-12-20 20:34:07 +0200

    add base structure + file

---
+++ src/js/js-components/burger.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger');
+const burgerLine = document.querySelector('.burger__line');
+
+export const burgerOpen = () => {
+	burger.addEventListener('click', () => {
+		burgerLine.classList.toggle('line--hide');
+		burger.classList.toggle('burger--transform');
+	});
+};
