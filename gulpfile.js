a4d41c35320a0e7d7f834d2590c30821c5afee2c
unknown <d.kazarov1361@gmail.com>
2024-12-20 22:15:06 +0200

    add task style

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-const { src, dest } = require('gulp');
+const { series, parallel, watch } = require('gulp');
 
 const fileincludes = require('./tasks/fileinclude');
+const style = require('./tasks/style');
 
+exports.style = style;
 exports.fileincludes = fileincludes;
+
+exports.default = series(style, fileincludes);
